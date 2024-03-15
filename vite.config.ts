73fc0ed166ee6f8904f5eb03754e11aa0f1d1cfb
fdkevin <fdkevinzero@gmail.com>
2024-03-15 19:12:23 +0800

    feat: add vite visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import path from "path";
 
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { visualizer } from "rollup-plugin-visualizer";
+import { PluginOption, defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer() as PluginOption,
+  ],
   resolve: {
     dedupe: ["react", "react-dom"],
     alias: {
